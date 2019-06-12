d9fa137b64f4bdb5925ceaf79d62495c53578d25
Franz-Richard Kunze <franz-richard.kunze@sennder.com>
2019-06-12 14:03:30 +0200

    simplify tests

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -11,29 +11,29 @@ class TestFieldIsAccessible(TestCase):
         self.collection = Collection()
 
     def test_not_accessible_if_no_fields(self):
-        setattr(self.collection, "_fields", {})
+        self.collection._fields = {}
 
         assert not self.collection.field_is_accessible(child=CHILD)
 
     def test_not_accessible_if_child_not_in_fields(self):
-        setattr(self.collection, "_fields", {CHILD})
+        self.collection._fields = {CHILD}
 
         assert not self.collection.field_is_accessible(child="something else")
 
     def test_is_accessible_if_child_in_fields_and_no_permitted_fields(self):
-        setattr(self.collection, "_fields", {CHILD})
-        setattr(self.collection, "permitted_fields", None)
+        self.collection._fields = {CHILD}
+        self.collection.permitted_fields = None
 
         assert self.collection.field_is_accessible(child=CHILD)
 
     def test_is_accessible_if_child_in_permitted_fields(self):
-        setattr(self.collection, "_fields", {CHILD})
-        setattr(self.collection, "permitted_fields", {CHILD})
+        self.collection._fields = {CHILD}
+        self.collection.permitted_fields = {CHILD}
 
         assert self.collection.field_is_accessible(child=CHILD)
 
     def test_not_accessible_if_child_not_in_permitted_fields(self):
-        setattr(self.collection, "_fields", {CHILD})
-        setattr(self.collection, "permitted_fields", {"some other field"})
+        self.collection._fields = {CHILD}
+        self.collection.permitted_fields = {"some other field"}
 
         assert not self.collection.field_is_accessible(child=CHILD)
